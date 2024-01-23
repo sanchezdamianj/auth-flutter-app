a074bb4504da8aebf0a712821db5fcb49c5976c8
Damian Sanchez <damian.sanchez@itti.digital>
2024-01-22 21:48:11 -0300

    creating a products provider using RIverPOd

---
+++ lib/features/products/presentation/providers/products_provider.dart
@@ -0,0 +1,77 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:teslo_shop/features/products/domain/domain.dart';
+import 'products_repository_provider.dart';
+
+//StateProvider
+
+class ProductsState {
+  final bool isLastPage;
+  final int limit;
+  final int offset;
+  final bool isLoading;
+  final List<Product> products;
+
+  ProductsState({
+    this.isLastPage = false,
+    this.limit = 10,
+    this.offset = 0,
+    this.isLoading = false,
+    this.products = const [],
+  });
+
+  ProductsState copyWith({
+    bool? isLastPage,
+    int? limit,
+    int? offset,
+    bool? isLoading,
+    List<Product>? products,
+  }) =>
+      ProductsState(
+        isLastPage: isLastPage ?? this.isLastPage,
+        limit: limit ?? this.limit,
+        offset: offset ?? this.offset,
+        isLoading: isLoading ?? this.isLoading,
+        products: products ?? this.products,
+      );
+}
+
+// State Notifier Provider
+
+class ProductNotifier extends StateNotifier<ProductsState> {
+  final ProductsRepository productsRepository;
+  ProductNotifier({required this.productsRepository}) : super(ProductsState()) {
+    //When the first instance is created, we call loadNextPage;
+    loadNextPage();
+  }
+
+  Future loadNextPage() async {
+    //if we dont have more pages to load, we return do nothing.
+    if (state.isLoading || state.isLastPage) {
+      return;
+    }
+
+    state = state.copyWith(
+      isLoading: true,
+    );
+    final products = await productsRepository.getProductByPage(
+        state.limit, state.offset + state.limit);
+
+    if (products.isEmpty) {
+      state = state.copyWith(isLoading: false, isLastPage: true);
+      return;
+    }
+    state = state.copyWith(
+      isLoading: false,
+      isLastPage: false,
+      offset: state.offset + state.limit,
+      products: [...state.products, ...products],
+    );
+  }
+}
+
+//Provider del State Notifier que ata a la instancia de ProductsNotifier con el State
+final productsProvider =
+    StateNotifierProvider<ProductNotifier, ProductsState>((ref) {
+  final productsRepository = ref.watch(productsRepositoryProvider);
+  return ProductNotifier(productsRepository: productsRepository);
+});
---
+++ lib/features/products/presentation/providers/products_repository_provider.dart
@@ -0,0 +1,13 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:teslo_shop/features/auth/presentation/providers/auth_provider.dart';
+import 'package:teslo_shop/features/products/domain/domain.dart';
+import 'package:teslo_shop/features/products/infrastructure/infrastructure.dart';
+
+//Provider readonly, what only change when the token has changed
+final productsRepositoryProvider = Provider<ProductsRepository>((ref) {
+  final accessToken = ref.watch(authProvider).user?.token ?? '';
+  final productRepository =
+      ProductsRepositoryImpl(ProductsDatasourceImpl(accessToken: accessToken));
+
+  return productRepository;
+});
